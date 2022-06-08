0c9952873386d481a0e69edcc20feb4be17c4bcf
fauch47 <69970845+fauch47@users.noreply.github.com>
2022-06-08 14:43:06 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function messageAfficher()
+{
+divInfo = document.getElementById('message');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'block';
+else
+divInfo.style.display = 'none'; }
